44fcfe984bee0fd3765b37c82e5167e8e7bee27e
Alex Zywicki <alexander.zywicki@loop.colum.edu>
2014-12-19 17:21:11 -0600

    0.0.29
    addded license to new files

--- src/DSGFile.h
+++ src/DSGFile.h
@@ -5,6 +5,22 @@
 //  Created by Alexander Zywicki on 12/19/14.
 //  Copyright (c) 2014 Alexander Zywicki. All rights reserved.
 //
+/*
+ This file is part of the Digital Signal Generation Project or “DSG”.
+
+ DSG is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ DSG is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with DSG.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __DSG__File__
 #define __DSG__File__
 #include <stdio.h>
@@ -16,7 +32,6 @@ namespace DSG{
         typedef enum File_Types{
             PlotFile = 0
         }File_Types;
-
         template<unsigned type> bool Write(std::string const& filename){
             return false;
         }
